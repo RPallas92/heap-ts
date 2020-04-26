38f7aae7bc6cd33ffb3bb19310c23b4f475581fa
Richi Pallas <ricardo.pallas@adidas-group.com>
2020-04-26 13:23:31 +0200

    Adds exports in index.ts

--- test/BinaryHeap.test.ts
+++ test/BinaryHeap.test.ts
@@ -7,60 +7,60 @@ const equalityFunction: Equality<number> = (a, b) => a === b;
 const items = [4, 5, 3, 8, 7, 23, 1, 456, 15, 9887, 3];
 
 describe("BinaryHeap", () => {
-	test("Creates a new one", () => {
-		const heap = new BinaryHeap(sortFunction, equalityFunction);
-		expect(heap).not.toBeUndefined();
-	});
+  test("Creates a new one", () => {
+    const heap = new BinaryHeap(sortFunction, equalityFunction);
+    expect(heap).not.toBeUndefined();
+  });
 
-	test("Creates a new one with initial items", () => {
-		const heap = new BinaryHeap(sortFunction, equalityFunction, items);
-		expect(heap).not.toBeUndefined();
-	});
+  test("Creates a new one with initial items", () => {
+    const heap = new BinaryHeap(sortFunction, equalityFunction, items);
+    expect(heap).not.toBeUndefined();
+  });
 
-	test("length() returns correct length", () => {
-		const heap = new BinaryHeap(sortFunction, equalityFunction, items);
-		expect(heap.length()).toEqual(items.length);
-	});
+  test("length() returns correct length", () => {
+    const heap = new BinaryHeap(sortFunction, equalityFunction, items);
+    expect(heap.length()).toEqual(items.length);
+  });
 
-	test("Push adds an item", () => {
-		const heap = new BinaryHeap(sortFunction, equalityFunction);
-		heap.push(1);
-		expect(heap.length()).toBe(1);
-	});
+  test("Push adds an item", () => {
+    const heap = new BinaryHeap(sortFunction, equalityFunction);
+    heap.push(1);
+    expect(heap.length()).toBe(1);
+  });
 
-	test("Peek returns highest item without removing it", () => {
-		const heap = new BinaryHeap(sortFunction, equalityFunction);
-		heap.push(5);
-		expect(heap.peek()).toBe(5);
-		expect(heap.length()).toBe(1);
-	});
+  test("Peek returns highest item without removing it", () => {
+    const heap = new BinaryHeap(sortFunction, equalityFunction);
+    heap.push(5);
+    expect(heap.peek()).toBe(5);
+    expect(heap.length()).toBe(1);
+  });
 
-	test("Pop returns highest item removing it", () => {
-		const heap = new BinaryHeap(sortFunction, equalityFunction);
-		heap.push(10);
-		heap.push(5);
-		expect(heap.length()).toBe(2);
-		expect(heap.pop()).toBe(5);
-		expect(heap.length()).toBe(1);
-	});
+  test("Pop returns highest item removing it", () => {
+    const heap = new BinaryHeap(sortFunction, equalityFunction);
+    heap.push(10);
+    heap.push(5);
+    expect(heap.length()).toBe(2);
+    expect(heap.pop()).toBe(5);
+    expect(heap.length()).toBe(1);
+  });
 
-	test("Removes removes item", () => {
-		const heap = new BinaryHeap(sortFunction, equalityFunction);
-		heap.push(10);
-		heap.push(5);
-		heap.remove(5);
-		expect(heap.length()).toBe(1);
-		expect(heap.pop()).toBe(10);
-	});
+  test("Removes removes item", () => {
+    const heap = new BinaryHeap(sortFunction, equalityFunction);
+    heap.push(10);
+    heap.push(5);
+    heap.remove(5);
+    expect(heap.length()).toBe(1);
+    expect(heap.pop()).toBe(10);
+  });
 
-	test("Pops all items in correct order", () => {
-		const expectedItems = [1, 3, 3, 4, 5, 7, 8, 15, 23, 456, 9887];
-		const heap = new BinaryHeap(sortFunction, equalityFunction, items);
-		const sortedItems = [];
-		while (heap.length() > 0) {
-			sortedItems.push(heap.pop());
-		}
-		expect(heap.length()).toBe(0);
-		expect(sortedItems).toEqual(expectedItems);
-	});
+  test("Pops all items in correct order", () => {
+    const expectedItems = [1, 3, 3, 4, 5, 7, 8, 15, 23, 456, 9887];
+    const heap = new BinaryHeap(sortFunction, equalityFunction, items);
+    const sortedItems = [];
+    while (heap.length() > 0) {
+      sortedItems.push(heap.pop());
+    }
+    expect(heap.length()).toBe(0);
+    expect(sortedItems).toEqual(expectedItems);
+  });
 });
