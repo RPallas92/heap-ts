86556bdd53c01e625b79c166427f74e8a57f47db
Richi Pallas <ricardo.pallas@adidas-group.com>
2020-04-26 16:26:14 +0200

    Creates scripts to publish as npm package

--- out/src/BinaryHeap.d.ts
+++ out/src/BinaryHeap.d.ts
@@ -2,22 +2,26 @@ import { Heap } from "./Heap";
 import { Sort } from "./Sort";
 import { Equality } from "./Equality";
 export declare class BinaryHeap<T> implements Heap<T> {
-    private items;
-    private sortFunction;
-    private equalityFunction;
-    constructor(sortFunction: Sort<T>, equalityFunction: Equality<T>, items?: T[]);
-    peek(): T | undefined;
-    push(item: T): void;
-    pop(): T | undefined;
-    remove(item: T): void;
-    length(): number;
-    private siftUp;
-    private siftDown;
-    private swap;
-    private needsToSwap;
-    private getItem;
-    private getParentIndex;
-    private getLeftChildIndex;
-    private getRightChildIndex;
-    private indexExists;
+  private items;
+  private sortFunction;
+  private equalityFunction;
+  constructor(
+    sortFunction: Sort<T>,
+    equalityFunction: Equality<T>,
+    items?: T[]
+  );
+  peek(): T | undefined;
+  push(item: T): void;
+  pop(): T | undefined;
+  remove(item: T): void;
+  length(): number;
+  private siftUp;
+  private siftDown;
+  private swap;
+  private needsToSwap;
+  private getItem;
+  private getParentIndex;
+  private getLeftChildIndex;
+  private getRightChildIndex;
+  private indexExists;
 }
